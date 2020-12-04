1d413f5140a6efb5d5afaa0667cefab7b9b2ebfa
Marcos <nyrocha018@gmail.com>
2020-12-04 09:01:55 -0300

    configuração da aplicação

--- src/main/java/capela/projeto/data/service/ServiceInterface.java
+++ src/main/java/capela/projeto/data/service/ServiceInterface.java
@@ -1,18 +1,11 @@
 package capela.projeto.data.service;
 
-
-import static capela.projeto.web.exception.ControllerException.notFound;
-import static capela.projeto.web.exception.ControllerException.*;
-import static java.lang.String.*;
-import static org.springframework.data.jpa.domain.Specification.*;
-import static org.springframework.http.ResponseEntity.notFound;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
-
 
 @Service
 public interface ServiceInterface<T, ID> {
